71edc2581d88579df778f08b1e263a8496c7fa9d
rovaimsiraj2007-creator <rovaimsiraj2007@gmail.com>
2025-12-30 22:07:22 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let Tableprint = prompt("Which table do you want to print?");
 let Limit = prompt("Up to which number do you want to print it?");
+alert("Your table has been printed in the console!")
 
 for(let i = 1; i <= Limit; i++){
     console.log(Tableprint + " x " + i + " = " + i * Tableprint);
+    
 }
