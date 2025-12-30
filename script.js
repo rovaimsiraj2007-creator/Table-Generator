90e7229b773fa4d832827504fb422b5c145ebb3a
rovaimsiraj2007-creator <rovaimsiraj2007@gmail.com>
2025-12-30 22:00:49 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let Tableprint = prompt("Table");
-let Limit = prompt("Limit");
+let Tableprint = prompt("Which table do you want to print?");
+let Limit = prompt("Up to which number do you want to print it?");
 
 for(let i = 1; i <= Limit; i++){
     console.log(Tableprint + " x " + i + " = " + i * Tableprint);
